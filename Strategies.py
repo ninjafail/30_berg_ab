59e1d7dcfef5048f220552ab0d0e89636f15ac82
Florian Romann thinkpad_fedora <s8flroma@stud.uni-saarland.de>
2022-09-26 20:15:44 +0200

    new strategy

--- Strategies.py
+++ Strategies.py
@@ -1,21 +1,25 @@
 from Player import Player
 from abc import abstractmethod
 
+
 class Strategy(Player):
     def __init__(self, name):
         super().__init__(name)
 
+    @staticmethod
+    def get_all_n(number: int, roll: list[int]):
+        return [i for i, r in enumerate(roll) if r == number]
+
     def strategy(self, roll: list[int]) -> list[int]:
         res = []
-        for i, r in enumerate(roll):
-            self.take_dice(res, i, r, roll)
+        self.take_dice(res, roll)
 
         if not res:
             res.append(roll.index(max(roll)))
         return res
 
     @abstractmethod
-    def take_dice(self, res: list[int], i: int, r: int, roll: list[int]):
+    def take_dice(self, res: list[int], roll: list[int]):
         pass
 
 
@@ -23,16 +27,26 @@ class StratSix(Strategy):
     def __init__(self, name="StratSix"):
         super().__init__(name)
 
-    def take_dice(self, res: list[int], i: int, r: int, roll: list[int]):
-        if r == 6:
-            res.append(i)
+    def take_dice(self, res: list[int], roll: list[int]):
+        res += self.get_all_n(6, roll)
 
 
 class StratFiveOrSix(Strategy):
-    def __init__(self, name="StratSix"):
+    def __init__(self, name="StratFiveOrSix"):
         super().__init__(name)
 
-    def take_dice(self, res: list[int], i: int, r: int, roll: list[int]):
-        if r == 5 or r == 6:
-            res.append(i)
+    def take_dice(self, res: list[int], roll: list[int]):
+        res += self.get_all_n(6, roll)
+        res += self.get_all_n(5, roll)
 
+
+class StratSixWithEndLess(Strategy):
+    def __init__(self, name="StratSixWithEndLess"):
+        super().__init__(name)
+
+    def take_dice(self, res: list[int], roll: list[int]):
+        res += self.get_all_n(6, roll)
+        if len(roll) - len(res) < 3:
+            res += self.get_all_n(5, roll)
+        if len(roll) - len(res) < 2:
+            res += self.get_all_n(4, roll)
--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 from Game import Game
 from Player import Player, HumanPlayer
-from Strategies import Strategy, StratSix, StratFiveOrSix
+from Strategies import Strategy, StratSix, StratFiveOrSix, StratSixWithEndLess
 from Simulator import Simulator
 
 
-def compare(strategy1: Strategy, strategy2: Strategy):
+def compare(strategy1: Strategy, strategy2: Strategy, iterations=10000):
     sim1 = Simulator(strategy1)
-    sim1.plot_turn_performance()
-    Simulator(strategy2).plot_turn_performance()
-    sim1.plot_cmp_with_strategy(strategy2, iterations=100000)
+    sim1.plot_turn_performance(iterations)
+    Simulator(strategy2).plot_turn_performance(iterations)
+    sim1.plot_cmp_with_strategy(strategy2, iterations)
 
 
 def play_test_game():
     names = ["Ben Dover", "Hugh Jazz", "Mike Litoris"]
     playersai = [StratSix('AI'), StratSix("AIGuenther"), StratSix('AI2')]
     players = [HumanPlayer(name) for name in names]
-    Game(playersai).play()
+    Game(players).play()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    compare(StratSix(), StratFiveOrSix())
-
-
-
+    compare(StratSixWithEndLess(), StratFiveOrSix(), 100000)
+    # play_test_game()
